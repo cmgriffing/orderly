08f861eb8aad7ac001c9c02d78136d9d4119a8aa
Chris Griffing <cmgriffing@gmail.com>
2024-01-17 15:08:03 -0800

    fix: always use base

--- src/router.tsx
+++ src/router.tsx
@@ -1,28 +1,33 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import { App } from "./App";
 import { Demo } from "./Demo";
 import { Home } from "./routes/Home";
 import { Chapter } from "./routes/Chapter";
 import { Snippet } from "./routes/Snippet";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "",
+          element: <Home />,
+        },
+        {
+          path: "books/:bookId/chapters/:chapterId",
+          element: <Chapter />,
+          children: [{ path: "snippets/:snippetId", element: <Snippet /> }],
+        },
+      ],
+    },
+    {
+      path: "/demo",
+      element: <Demo />,
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "books/:bookId/chapters/:chapterId",
-        element: <Chapter />,
-        children: [{ path: "snippets/:snippetId", element: <Snippet /> }],
-      },
-    ],
-  },
-  {
-    path: "/demo",
-    element: <Demo />,
-  },
-]);
+    basename: "/orderly",
+  }
+);
