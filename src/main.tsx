1687e2e54c8b1824e5004a922e5e81ba53f2fdf7
Chris Griffing <cmgriffing@gmail.com>
2024-01-17 16:56:08 -0800

    fix: add suspense and loader to prevent navigation bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, Loader, Flex } from "@mantine/core";
 
 import "@mantine/core/styles.css";
 import "./index.css";
@@ -10,7 +10,15 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <MantineProvider>
-      <RouterProvider router={router} />
+      <Suspense
+        fallback={
+          <Flex h="100vh" w="100vw" align={"center"} justify={"center"}>
+            <Loader />
+          </Flex>
+        }
+      >
+        <RouterProvider router={router} />
+      </Suspense>
     </MantineProvider>
   </React.StrictMode>
 );
